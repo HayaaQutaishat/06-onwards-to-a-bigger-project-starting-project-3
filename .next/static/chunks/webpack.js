dfa6a9569291469f9b0e64cbf611307681fed6ed
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2023-02-08 15:38:35 +0100

    Create Home Page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4d4b5627c89203bcce3c";
+/******/ 	var hotCurrentHash = "4e87ae09696ecaa45bf2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
