81d64b5f91b2feee48fad246221ecd4887bcea95
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2023-02-08 16:07:30 +0100

    Programmatic Navigation using "useRouter" Hook

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5b6ccce4d45b83f05feb";
+/******/ 	var hotCurrentHash = "5894efdec94b7aa677c0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
