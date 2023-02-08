64cef1563229ffdfc50ba53ee7e0cdf058664607
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2023-02-08 16:52:12 +0100

    Adding CSS Modules

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "02a567ec9bb63d219662";
+/******/ 	var hotCurrentHash = "6210e1c00022d5f4feb9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
