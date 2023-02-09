1200262d8aafa83de365ca14ee3fab2dcef4059d
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2023-02-09 09:58:25 +0100

    Programmatically Navigate Users Away "useRouter"

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "02933d6c7e39f2c3787c";
+/******/ 	var hotCurrentHash = "b5a4408bfe5668ea497f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
