79a4c039560cb03424c653e87b0617955422ab81
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2023-02-09 10:49:02 +0100

    Adding "head" Metadata to the Home Page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f3632e36ef043d897a4d";
+/******/ 	var hotCurrentHash = "f23d02e40f535f5396eb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
